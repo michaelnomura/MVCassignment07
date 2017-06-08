1049bc93f98a927c86a6a684be7ca0325995eace
michaelnomura <mnnomura@gmail.com>
2017-06-08 11:43:59 -0700

    Add files via upload

---
+++ MVCassignment07/Models/PersonLite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCassignment07.Models
+{
+    public class PersonLite
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string Apartment { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string HomePhone { get; set; }
+        
+    }
+}
